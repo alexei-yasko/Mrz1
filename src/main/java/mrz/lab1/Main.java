11059698c8cc9458fc64112e389209669db8a8dd
Yasko Alexei <alexei.yasko@gmail.com>
2013-02-01 12:38:35 +0300

    output improved

--- src/main/java/mrz/lab1/Main.java
+++ src/main/java/mrz/lab1/Main.java
@@ -2,6 +2,7 @@ package mrz.lab1;
 
 import java.util.List;
 
+import mrz.lab1.binary.BinaryNumber;
 import mrz.lab1.processor.ProcessingItem;
 import mrz.lab1.processor.Processor;
 
@@ -12,15 +13,23 @@ public class Main {
 
     public static void main(String[] args) {
         String dataFile = args[0];
-//        String dataFile = "data/data.txt";
+        //String dataFile = "data/data.txt";
 
         Processor processor = new Processor(dataFile);
         List<ProcessingItem> result = processor.processData();
 
-        System.out.println("\n------------------------- Result -------------------------\n");
+        StringBuilder stringBuilder = new StringBuilder("\n------------------------- Result -------------------------\n");
         for (ProcessingItem processingItem : result) {
-            System.out.println(processingItem.getFactor() + "\n" + processingItem.getMultiplicand() + "\n" +
-                processingItem.getPartialSum() + "\n");
+            stringBuilder.append("\n").append(processingItem.getFactor()).append(" (2) = ")
+                .append(BinaryNumber.toInt(processingItem.getFactor())).append(" (10)\n")
+
+                .append(processingItem.getMultiplicand()).append(" (2) = ")
+                .append(BinaryNumber.toInt(processingItem.getMultiplicand())).append(" (10)\n")
+
+                .append(processingItem.getPartialSum()).append(" (2) = ")
+                .append(BinaryNumber.toInt(processingItem.getPartialSum())).append(" (10)\n");
         }
+
+        System.out.println(stringBuilder.toString());
     }
 }
